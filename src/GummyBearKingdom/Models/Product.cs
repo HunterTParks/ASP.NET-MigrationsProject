c7f461d72e922601c853c417131c9964bbc37591
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-04 08:12:04 -0700

    add a Product class with properties

---
+++ src/GummyBearKingdom/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GummyBearKingdom.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId;
+        public string Name;
+        public int CountryId;
+        public virtual Country Country { get; set; }
+    }
+}
