fb939594c6e49a46d9f299642fa77b3783f42183
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-04 08:08:08 -0700

    add a country class to use in a 'one-to-many' relationship in the database

---
+++ src/GummyBearKingdom/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummyBearKingdom.Models
+{
+    public class Country
+    {
+    }
+}
