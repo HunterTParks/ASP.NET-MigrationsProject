47a57a42137b0e3ba85790567755b29986ebb5ef
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-04 10:00:55 -0700

    add settings into Startup.cs to have the Home Route come from the HomeController. Also adds packages necessary to run program

--- src/GummyBearKingdom/Startup.cs
+++ src/GummyBearKingdom/Startup.cs
@@ -27,6 +27,7 @@ namespace GummyBearKingdom
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddEntityFramework()
                 .AddDbContext<GMDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
@@ -34,6 +35,13 @@ namespace GummyBearKingdom
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}")
+            });
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Waiting......");
