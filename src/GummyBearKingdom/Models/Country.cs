5024a7b32d95da21953980ebd5ab053e3cb7cf57
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-04 08:10:15 -0700

    add properties to Country class

--- src/GummyBearKingdom/Models/Country.cs
+++ src/GummyBearKingdom/Models/Country.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GummyBearKingdom.Models
 {
     public class Country
     {
+        [Key]
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
