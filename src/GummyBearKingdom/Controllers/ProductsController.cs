c4e6d60877a170f785fbd2990a94e142bbd37b8e
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-04 13:13:09 -0700

    add a create View page and editted Products Controller to become a bit drier

--- src/GummyBearKingdom/Controllers/ProductsController.cs
+++ src/GummyBearKingdom/Controllers/ProductsController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using GummyBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GummyBearKingdom.Controllers
 {
@@ -17,10 +13,11 @@ namespace GummyBearKingdom.Controllers
         {
             return View(db.Products.ToList());
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 
-    public IActionResult Create()
-    {
-        return View();
-    }
 }
