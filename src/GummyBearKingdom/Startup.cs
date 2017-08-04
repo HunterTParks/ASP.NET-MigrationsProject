27ce9edbcbdff4fb883b9728d255586e1e916962
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-04 08:38:30 -0700

    add specific libraries and remove unnecessary libraries to our Startup

--- src/GummyBearKingdom/Startup.cs
+++ src/GummyBearKingdom/Startup.cs
@@ -6,7 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using GummyBearKingdom.Models;
 
 namespace GummyBearKingdom
 {
