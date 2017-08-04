7a26c6c3e96772e69e9dfb505cc117e46d86b3da
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-04 08:36:27 -0700

    add DbContext class to have our code talk to our database

--- src/GummyBearKingdom/Models/GMDbContext.cs
+++ src/GummyBearKingdom/Models/GMDbContext.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace GummyBearKingdom.Models
 {
-    public class GMDbContext
+    public class GMDbContext : DbContext
     {
+        public GMDbContext()
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=GAMING-PC;Database=GummyBearKingdom;integrated security=True");
+        }
+
+        public GMDbContext(DbContextOptions<GMDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
